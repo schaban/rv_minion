76adbc50beb4311347e5bfd2537ac9a90f561129
schaban <sergey.chaban@gmail.com>
2026-01-28 09:47:58 +0200

    only report invalid instruction if corresponding fault flag is clear;

--- minion_instrs.c
+++ minion_instrs.c
@@ -1140,7 +1140,9 @@ void minion_instr(MINION* pMi, uint32_t instr, uint32_t mode) {
 	int is32 = ((instr & 3) == 3) && (op1 != 7);
 	if (!is32) {
 		if (pMi) {
-			minion_err(pMi, "not an rv32g instruction: %X\n", instr);
+			if ((pMi->faultFlags & 1) == 0) {	
+				minion_err(pMi, "not an rv32g instruction: %X\n", instr);
+			}
 			pMi->faultFlags |= 1;
 			pMi->pcStatus = MINION_PCSTATUS_NATIVE;
 		}
